0b33641576f9169ab744fd514ee4f36fd189452f
Pedro Antunes <pedroabernardes11@gmail.com>
2024-11-27 10:24:37 -0300

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
-        <h1 className="text-6xl">The best Journal app, period.</h1>
-        <p className="text-2xl text-white/60">
+        <h1 className="text-6xl mb-4">The best Journal app, period.</h1>
+        <p className="text-2xl text-white/60 mb-4">
           This is the best app for trackin your mood through out your life. All
           you have to do is be honest.
         </p>
         <div>
-          <button>get started</button>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl ">
+              Get Started
+            </button>
+          </Link>
         </div>
       </div>
     </div>
